83461dd2ea0ce2844d430dc5c5862291fe8717f5
Hüseyin Kurban <huseyinkurban03@gmail.com>
2025-03-14 18:01:11 +0300

    Add project files.

---
+++ Services/Review/Reservation.Review/Services/IUserReviewService.cs
@@ -0,0 +1,14 @@
+﻿using Reservation.Review.Dtos;
+
+namespace Reservation.Review.Services
+{
+    public interface IUserReviewService
+    {
+        Task<List<ResultUserReviewDto>> GetAllUserReviewAsync();
+        Task CreateUserReviewAsync(CreateUserReviewDto createUserReviewDto);
+        Task UpdateUserReviewAsync(UpdateUserReviewDto updateUserReviewDto);
+        Task DeleteUserReviewAsync(int id);
+        Task<GetByIdUserReviewDto> GetUserReviewByIdAsync(int id);
+
+    }
+}
