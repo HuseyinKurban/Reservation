55526130223aa69354cf95bb23d70d85b08efc6a
Hüseyin Kurban <huseyinkurban03@gmail.com>
2025-03-14 18:01:11 +0300

    Add project files.

---
+++ Services/Review/Reservation.Review/Controllers/UserReviewsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Reservation.Review.Dtos;
+using Reservation.Review.Services;
+
+namespace Reservation.Review.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserReviewsController : ControllerBase
+    {
+        private readonly IUserReviewService _userReviewService;
+
+        public UserReviewsController(IUserReviewService userReviewService)
+        {
+            _userReviewService = userReviewService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllUserReview()
+        {
+            var value = await _userReviewService.GetAllUserReviewAsync();
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUserReview(CreateUserReviewDto createUserReviewDto)
+        {
+            await _userReviewService.CreateUserReviewAsync(createUserReviewDto);
+            return Ok("Ekleme Başarılı");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateUserReview(UpdateUserReviewDto updateUserReviewDto)
+        {
+            await _userReviewService.UpdateUserReviewAsync(updateUserReviewDto);
+            return Ok("Güncelleme Başarılı");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteUserReview(int id)
+        {
+            await _userReviewService.DeleteUserReviewAsync(id);
+            return Ok("Silme Başarılı");
+        }
+
+        [HttpGet("id")]
+        public async Task<IActionResult> GetUserReview(int id)
+        {
+            var value = await _userReviewService.GetUserReviewByIdAsync(id);
+            return Ok(value);
+        }
+    }
+}
