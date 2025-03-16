fab454aa2d3918b3830c665d72a80e677ec235fd
Hüseyin Kurban <huseyinkurban03@gmail.com>
2025-03-17 02:53:54 +0300

    Add project files.

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -9,8 +10,8 @@ namespace IdentityServer
 {
     public static class Config
     {
-      public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
-      {
+        public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
+        {
           new ApiResource("ResourceBooking"){Scopes = {"BookingFullPermission"}},
           new ApiResource("ResourceBooking2"){Scopes = {"BookingReadPermission"}},
           new ApiResource("ResourceBooking3"){Scopes = {"BookingReadPermission","BookingUpdatePermission"}},
@@ -18,7 +19,57 @@ namespace IdentityServer
           new ApiResource("ResourcePayment"){Scopes = {"PaymentFullPermission"}},
           new ApiResource("ResourceBasket"){Scopes = {"BasketFullPermission"}},
           new ApiResource("ResourceMessage"){Scopes = {"MessageFullPermission"}},
+          new ApiResource(IdentityServerConstants.LocalApi.ScopeName) 
+                
+        };
 
-      };
+        public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email(),
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
+            {
+            new ApiScope("BookingFullPermission","Full Authority For Booking Operations"),
+            new ApiScope("BookingReadPermission","Reading Authority For Booking Operations"),
+            new ApiScope("BookingUpdatePermission","Update Authority For Booking Operations"),
+            new ApiScope("ReviewFullPermission","Full Authority For Review Operations"),
+            new ApiScope("PaymentFullPermission","Full Authority For Payment Operations"),
+            new ApiScope("BasketFullPermission","Full Authority For Basket Operations"),
+            new ApiScope("MessageFullPermission","Full Authority For Message Operations"),
+            new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
+        };
+
+        public static IEnumerable<Client> Clients => new Client[]
+        {
+            new Client
+            {
+                ClientId = "BookingVisitorId",
+                ClientName= "Booking Visitor User",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = { new Secret("reservationsecret".Sha256()) },
+                AccessTokenLifetime = 600,
+                AllowedScopes = { "BookingReadPermission" },
+                AllowAccessTokensViaBrowser = true
+            },
+
+            new Client
+            {
+                ClientId = "ReservationAdminId",
+                ClientName= "Reservation Admin User",
+                AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                ClientSecrets = { new Secret("reservationsecret".Sha256()) },
+                AllowedScopes = { "BookingFullPermission", "BookingReadPermission", "BookingUpdatePermission","ReviewFullPermission","PaymentFullPermission","BasketFullPermission","MessageFullPermission",
+                IdentityServerConstants.LocalApi.ScopeName,
+                IdentityServerConstants.StandardScopes.Email,
+                IdentityServerConstants.StandardScopes.OpenId,
+                IdentityServerConstants.StandardScopes.Profile
+                },
+                AccessTokenLifetime = 600,
+             
+            },
+        };
     }
 }
---
+++ IdentityServer/Dtos/UserRegisterDto.cs
@@ -0,0 +1,10 @@
+﻿namespace IdentityServer.Dtos
+{
+    public class UserRegisterDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+    }
+}
