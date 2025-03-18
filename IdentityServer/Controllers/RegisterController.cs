4646b26829130abafdcff0bb7bc57ab3b57590df
Hüseyin Kurban <huseyinkurban03@gmail.com>
2025-03-18 21:51:03 +0300

    Add project files.

---
+++ IdentityServer/Controllers/RegisterController.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using IdentityServer.Dtos;
+using IdentityServer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegisterController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateRegister(UserRegisterDto userRegisterDto)
+        {
+            var values = new ApplicationUser
+            {
+                Name = userRegisterDto.Name,
+                Surname = userRegisterDto.Surname,
+                UserName = userRegisterDto.Username,
+                Email = userRegisterDto.Email
+            };
+            var result = await _userManager.CreateAsync(values, userRegisterDto.Password);
+
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+            return StatusCode(StatusCodes.Status500InternalServerError, new { Status = "Error", Message = "User creation failed! Please check user details and try again." });
+        }
+    }
+}
